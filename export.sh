e4bf5ad072455f81eed42acccef2fab6c556d867
Nocturnal <contact@nocturnal.solutions>
2018-05-18 13:48:35 -0600

    Update export script to not halt the git push if the book exporting fails

--- export.sh
+++ export.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env csh
 # Script to generate ebooks
+# Call from .git/hooks/pre-push
 setenv MDLIST "README.md `ls README.md *.md appendices/*.md | sed -E -e 's/(README|SUMMARY).md//g' | paste -s -d ' ' -`"
 setenv TOCSETS "--toc --toc-depth=2"
 setenv PDFSETS "$TOCSETS --include-in-header=tex-addons.tex -V fontfamily:bookman --listings"
@@ -9,3 +10,5 @@ pandoc -o ebooks/Kitura-Until-Dawn.epub --epub-metadata=epub-metadata.xml --epub
 pandoc -o ebooks/Kitura-Until-Dawn.docx $TOCSETS $MDLIST && echo "Word exported"
 # KindleGen downloaded from https://www.amazon.com/gp/feature.html?docId=1000765211
 kindlegen ebooks/Kitura-Until-Dawn.epub && echo "Mobipocket exported"
+# Exit with code 0 so push continues even if building failed
+exit 0
