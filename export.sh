d18ff3c61f8872878cac805ce13e4e00922c1b12
Nocturnal <contact@nocturnal.solutions>
2018-05-21 10:31:39 -0600

    Add RTF export.

--- export.sh
+++ export.sh
@@ -8,6 +8,7 @@ pandoc -o ebooks/Kitura-Until-Dawn.pdf -V papersize:letter $PDFSETS $MDLIST && e
 pandoc -o ebooks/Kitura-Until-Dawn-A4.pdf -V papersize:A4 $PDFSETS $MDLIST && echo "A4 size PDF exported"
 pandoc -o ebooks/Kitura-Until-Dawn.epub --epub-metadata=epub-metadata.xml --epub-cover-image=images/logo-vert.png $TOCSETS $MDLIST && echo "Epub exported"
 pandoc -o ebooks/Kitura-Until-Dawn.docx $TOCSETS $MDLIST && echo "Word exported"
+pandoc -o ebooks/Kitura-Until-Dawn.rtf -s $TOCSETS $MDLIST && echo "RTF exported"
 # KindleGen downloaded from https://www.amazon.com/gp/feature.html?docId=1000765211
 kindlegen ebooks/Kitura-Until-Dawn.epub && echo "Mobipocket exported"
 # Exit with code 0 so push continues even if building failed
